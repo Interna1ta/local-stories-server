db5b5965b25d407c0736688c67f6017e942d7261
Pere Serra Bosch <pereserrabosch@MacBook-Pro-de-Pere.local>
2018-05-17 12:58:35 +0200

    errors when you tweet

--- routes/twitter.js
+++ routes/twitter.js
@@ -6,12 +6,13 @@ const router = express.Router();
 const Twitter = require('twitter');
 
 router.post('/', (req, res, next) => {
+   // @todo chek if user loggend in 
 
   const client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_API_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY, // @todo twitter req.session.currentUser.tokenKey,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET // @todo twitter req.session.current.tokenSecret
   });
   
   const tweet = req.body.text;
@@ -24,12 +25,9 @@ router.post('/', (req, res, next) => {
 
   client.post('statuses/update', { status: tweet })
     .then((tweet) => {
-      console.log(tweet);
+      res.status(204).send();
     })
-    .catch((error) => {
-      throw error;
-    })
-
+    .catch(next)
 });
 
 // router.get('/login', passport.authenticate('twitter'), (req, res, next) => {
